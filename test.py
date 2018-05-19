5e47701524b5b85a98be37dbb772ccbb66486b51
dorcasandayi <dorcasandayi@gmail.com>
2018-05-19 12:54:36 +0300

    add,minus,product

---
+++ test.py
@@ -0,0 +1,12 @@
+num1 = 4
+num2 = 16
+num3 = 12
+
+calc = {}
+sum = num1 + num2
+product = num2 * num3
+minus = num3 - num1
+
+calc["addition"] = sum
+calc["product"] = product
+calc["minus"] = minus
